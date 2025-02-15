9d6b7fe71f4465f6bea38a615ee823f0cb3fd183
Maruyou <norio_shibata@maru-you.co.jp>
2025-02-16 01:35:56 +0900

    Fix: Update next.config.js for Vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // これを追加！
+  experimental: {
+    appDir: true, // App Router (appディレクトリ) を有効化
+  },
+  output: 'standalone', // Vercelで正しく動作するように
 };
 
 module.exports = nextConfig;
